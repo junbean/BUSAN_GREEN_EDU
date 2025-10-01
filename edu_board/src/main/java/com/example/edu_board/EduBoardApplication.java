b0a3c6ed165dac97811bdb03ff0e529fb71d91ed
junbean <60472117+junbean@users.noreply.github.com>
2025-10-01 09:29:46 +0900

    board project create
    2025.10.01 게시판 프로젝트 생성

---
+++ edu_board/src/main/java/com/example/edu_board/EduBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.edu_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduBoardApplication.class, args);
+	}
+
+}
---
+++ edu_board/src/main/java/com/example/edu_board/controller/MainController.java
@@ -0,0 +1,4 @@
+package com.example.edu_board.controller;
+
+public class MainController {
+}
